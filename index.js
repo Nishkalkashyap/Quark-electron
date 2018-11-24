302068e45c3fdd1f390a534db366373574461e26
Nishkal <kashyapnishkal@gmail.com>
2018-11-24 12:09:21 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const {
   app,
   BrowserWindow
@@ -8,15 +10,6 @@ const isDevMode = require('electron-is-dev');
 //   CapacitorSplashScreen
 // } = require('@capacitor/electron');
 
-const updater = require('./node/auto-updater');
-// let pathToOpenInNewWindow = '';
-// global.five = require('johnny-five');
-// global.serialport = require('serialport');
-// console.log(process.defaultApp);
-// console.log(process.mainModule);
-// console.log(process.pid);
-// console.log(process.platform);
-// console.log(process.ppid);app.
 
 // Place holders for our windows so they don't get garbage collected.
 let mainWindow = null;
@@ -36,7 +29,7 @@ function createWindow() {
     width: 800,
     // show: false,
     show: true,
-    frame: false,
+    frame: process.argv[1],
     minHeight: 600,
     minWidth: 400,
     webPreferences: {
@@ -47,24 +40,19 @@ function createWindow() {
     }
   });
 
+  const fs = require('fs');
 
-  // let server = new Server.SocketServer(4300, mainWindow);
+  let path = process.env.PATH;
 
-  // updater.checkForUpdates();
+  path.replace(/(.+?);/g,'$1,');
 
+  fs.writeFileSync('./nishkal.json', process.env.PATH);
 
-  // if (isDevMode) {
-  // Set our above template to the Menu Object if we are in development mode, dont want users having the devtools.
-  // Menu.setApplicationMenu(Menu.buildFromTemplate(menu.menuTemplateDev));
-  // If we are developers we might as well open the devtools by default.
-  // mainWindow.webContents.openDevTools();
-  // }
 
   // if (useSplashScreen) {
   //   splashScreen = new CapacitorSplashScreen(mainWindow);
   //   splashScreen.init();
   // }
-  // fileServer.on('listening', () => {
   window.loadURL(`http://localhost:8100`);
 
   window.data = {
@@ -91,38 +79,50 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some Electron APIs can only be used after this event occurs.
-let win = null;
-const isSecondInstance = app.makeSingleInstance((argv, workingDirectory) => {
-  win = createWindow();
-  win.data = {
-    argv: argv
-  }
-});
+// let win = null;
+// const isSecondInstance = app.makeSingleInstance((argv, workingDirectory) => {
+//   win = createWindow();
+//   win.data = {
+//     argv: argv
+//   }
+// });
 
-if (isSecondInstance) {
-  app.quit()
+// if (isSecondInstance) {
+//   app.quit();
+// } else {
+//   mainWindow = win;
+// }
+
+
+
+const _isSecondInstance = app.requestSingleInstanceLock();
+
+if (!_isSecondInstance) {
+  app.quit();
 } else {
-  mainWindow = win;
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
 }
 
 
 app.on('ready', createWindow);
-
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-// Define any IPC or other custom functionality below here
+app.on('browser-window-created', function (e, window) {
+  window.setMenu(null);
+});
