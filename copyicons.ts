be0fb8d9837a587223d8b8b998c894911c2e8f49
Nishkal <kashyapnishkal@gmail.com>
2019-02-08 20:23:08 +0530

    copy changes

--- copyicons.ts
+++ copyicons.ts
@@ -1,16 +1,68 @@
 import * as ncp from 'ncp';
-import * as fs from 'fs';
-// import * as ncp from 'node_modules/@types/ncp';
+import * as fs from 'fs-extra';
+import * as recc from 'recursive-readdir';
 
-ncp.ncp('./node_modules/', './definitions/', {
-    filter: (file) => {
-        return (fs.statSync(file).isDirectory() || file.includes('.d.ts') || file.endsWith('package.json')) && !file.replace('node_modules', '').includes('node_modules');
-    },
-    dereference: true
-}, (e) => {
-    console.log(e, 'hehe');
-});
+copyDefinitions();
+copyAssets();
 
-ncp.ncp('./../QuarkUMD/src/assets/', './www/assets/', (e) => {
-    console.log(e);
-});
+function copyDefinitions() {
+
+    const Package = fs.readJsonSync('./package.json');
+    const deps = Package.dependencies;
+    const dev = Package.devDependencies;
+    const all = Object.keys(Object.assign({}, deps, dev));
+
+    const includeFiles = ['@squirtle/api', '@types/firmata', '@types/fs-extra', '@types/johnny-five', '@types/node', '@types/serialport', 'electron']
+
+    all.map((val) => {
+
+        if (!includeFiles.includes(val))
+            return;
+
+        recc(`./node_modules/` + val, [
+            (file, stat) => {
+                return !stat.isDirectory() && !file.endsWith('.d.ts');
+            }
+        ], (e, _files) => {
+            if (e) {
+                console.log(e);
+            }
+            if (_files.length == 0)
+                return;
+            console.log(_files.length, val);
+            copyFiles();
+        });
+
+        function copyFiles() {
+            const mkdirp = require('mkdirp');
+            mkdirp('./definitions/' + val, (e) => {
+                if (e) {
+                    console.log(e);
+                }
+            });
+
+            ncp.ncp(`./node_modules/` + val, './definitions/' + val, {
+                filter: (file) => {
+                    return (
+                        ((fs.statSync(file).isDirectory() || file.includes('.d.ts') || file.endsWith('package.json'))
+                            && (!file.replace('node_modules', '').includes('node_modules')))
+                        && (file.search(/api[\\/]umd/) == -1)
+                    );
+                },
+                dereference: true
+            }, (e) => {
+                if (e) {
+                    console.log(e, 'failed');
+                }
+            });
+        }
+    });
+}
+
+function copyAssets() {
+    ncp.ncp('./../QuarkUMD/src/assets/', './www/assets/', (e) => {
+        console.log(e);
+    });
+}
+
+
