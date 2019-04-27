8b11d0961e9456c1a16c96ccf62ea51d5fac3cee
Nishkal <kashyapnishkal@gmail.com>
2019-04-27 17:30:31 +0530

    updated license scripts

--- scripts/license.ts
+++ scripts/license.ts
@@ -1,11 +1,11 @@
 
-import * as recc from 'recursive-readdir';
 import * as fs from 'fs-extra';
 import * as lic from 'license-checker';
 import * as Path from 'path';
-
+import * as recc from 'recursive-readdir';
 
 export function createLicenseFile() {
+    const includedKeys: { [name: string]: string } = {};
     lic.init({
         start: './',
         production: true
@@ -13,49 +13,57 @@ export function createLicenseFile() {
         if (err) {
             console.log(err, 'Errored');
         } else {
-            let str = '\r\nThis application bundles the following third-party packages in accordance with the following licenses:\r\n';
-            str = str.concat('\r\n----------------------------------------------------------------------------', '\r\n');
+            let mainLicenseFile = '\r\nThis application bundles the following third-party packages in accordance with the following licenses:\r\n';
+            mainLicenseFile = mainLicenseFile.concat('\r\n----------------------------------------------------------------------------', '\r\n');
+
+            let subLicenseFile = '\r\nThis application bundles the following third-party packages in accordance with the following licenses:\r\n';
+            subLicenseFile = subLicenseFile.concat('\r\n----------------------------------------------------------------------------', '\r\n');
+
+
             const keys = Object.keys(lic);
+
+            // generate license file to be bundled
             keys.map((key) => {
                 const pack = lic[key];
+                const packageNameWithoutVersion = key.replace(getVersionFromName(key), '');
+
+                if (includedKeys[packageNameWithoutVersion]) {
+                    return;
+                }
+
+                if (pack['path'].includes('squirtle')) {
+                    return;
+                }
+
                 if (pack.licenseFile) {
-                    str = str.concat(`\r\n\r\nPackage: ${key}`, '\r\n', `License: ${pack.licenses}`, '\r\n', `License Source: ${Path.basename(pack.licenseFile || '')}`, '\r\n');
-                    str = str.concat(`Source Text: \r\n\r\n${(fs.readFileSync(pack.licenseFile)).toString()}\r\n`);
-                    str = str.concat('\r\n----------------------------------------------------------------------------', '\r\n');
+                    includedKeys[packageNameWithoutVersion] = pack.repository;
+
+                    subLicenseFile = subLicenseFile.concat(`\r\nPackage: ${packageNameWithoutVersion};`);
+
+                    mainLicenseFile = mainLicenseFile.concat(`\r\n\r\nPackage: ${key}`, '\r\n', `License: ${pack.licenses}`, '\r\n', `License Source: ${Path.basename(pack.licenseFile || '')}`, '\r\n');
+                    mainLicenseFile = mainLicenseFile.concat(`Source Text: \r\n\r\n${(fs.readFileSync(pack.licenseFile)).toString()}\r\n`);
+                    mainLicenseFile = mainLicenseFile.concat('\r\n----------------------------------------------------------------------------', '\r\n');
                 }
             });
-            fs.writeFileSync('./ThirdPartyNotices.txt', str);
+            fs.writeFileSync('./ThirdPartyNotices.txt', mainLicenseFile);
+            // fs.writeFileSync('./SubLicenses.txt', subLicenseFile);
         }
     });
+
+    function getVersionFromName(name: string) {
+        return name.match(/@[0-9\.]+/)[0];
+    }
 }
 
-
-
-export function _createLicenseFile() {
-
-    const excludes = ['@squirtle']
-
-    recc('./node_modules/', [
-        (file, stat) => {
-            return !(stat.isDirectory() || file.endsWith('package.json')) || file.search(/@squirtle/) !== -1
-        }
-    ]).then((val) => {
-        console.log(`Found total : ${val.length} files`);
-        const noLicenseFiles: string[] = [];
-        val.map((path, index) => {
-            fs.readJson(path).then((json) => {
-                if (!json.license) {
-                    noLicenseFiles.push(path);
-                    console.log(path, 'no-license');
-                }
-
-                if (index == val.length - 1) {
-                    console.log(`Found total : ${noLicenseFiles.length} files with no license`);
-                }
-            }).catch((err) => {
-                console.log('Error');
-            });
+export function createLicenseFileeee() {
+    let files: string[] = [];
+    recc('./node_modules', [(file, stat) => {
+        return !(file.endsWith('package.json') || stat.isDirectory())
+    }], (err, _files) => {
+        files = _files.map((val, index, arr) => {
+            return Path.basename(Path.dirname(val));
         });
+        console.log(files);
     });
 }
 
