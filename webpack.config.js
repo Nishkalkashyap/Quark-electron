bf236b2c99a15c28ffe60081b33f7c40459cf845
Nishkal <kashyapnishkal@gmail.com>
2019-06-05 11:56:18 +0530

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    mode: 'production',
+    // mode: 'development',
+    entry: path.resolve('./index.ts'),
+    output: {
+        path: path.resolve('./'),
+        filename: 'index.js',
+    },
+    externals: [nodeExternals({
+        whitelist: [/@quarkjs.+/]
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: (file) => (
+                    /node_modules/.test(file) &&
+                    !/\.vue\.js/.test(file)
+                ),
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        "presets": [
+                            [
+                                ("@babel/preset-env"), {
+                                    "targets": {
+                                        "chrome": "69"
+                                    }
+                                }
+                            ],
+                            [("@babel/preset-typescript"), {
+                                isTSX: false,
+                                allExtensions: true
+                            }],
+                        ],
+                        "plugins": [
+                            ("@babel/plugin-proposal-class-properties"),
+                            ("@babel/plugin-proposal-object-rest-spread"),
+                            ("@babel/plugin-syntax-dynamic-import")
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    target: 'electron-main',
+    node: {
+        __dirname: false
+    }
+}
