ad32752911735c75e7f6fca880d179ed5c60fbbf
Nishkal <kashyapnishkal@gmail.com>
2019-06-07 01:11:23 +0530

    minor changes

--- scripts/test.ts
+++ scripts/test.ts
@@ -23,7 +23,7 @@ async function runTest() {
             // './build/linux-unpacked/quark' : process.platform == 'darwin' ?
             `./build/Quark-linux-x86_64-${version}.AppImage` : process.platform == 'darwin' ?
                 // './build/mac/Quark.app' : null;
-                `open ${path.resolve(`./build/mac/Quark.app`)}` : null;
+                `${path.resolve(`./build/mac/Quark.app`)}` : null;
                 // `open ${path.resolve(`./build/Quark-mac-${version}.dmg`)}` : null;
                 // `open ${path.resolve(`./build/Quark-mac-${version}.pkg`)}` : null;
 
