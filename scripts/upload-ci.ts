24de17f4867f0da680455571bc4b159bb38cd251
Nishkal <kashyapnishkal@gmail.com>
2019-06-09 01:39:56 +0530

    fixed build bud

--- scripts/upload-ci.ts
+++ scripts/upload-ci.ts
@@ -18,10 +18,10 @@ const paths = [
     `./build/Quark-win-x64-${version}.zip`,
     './build/latest.yml',
 
-    `./../Quark-electron-linux/build/Quark-linux-amd64-${version}.deb`,
-    `./../Quark-electron-linux/build/Quark-linux-x64-${version}.tar.gz`,
-    `./../Quark-electron-linux/build/Quark-linux-x86_64-${version}.AppImage`,
-    `./../Quark-electron-linux/build/latest-linux.yml`,
+    `./build/Quark-linux-amd64-${version}.deb`,
+    `./build/Quark-linux-x64-${version}.tar.gz`,
+    `./build/Quark-linux-x86_64-${version}.AppImage`,
+    `./build/latest-linux.yml`,
 ]
 
 paths.map((_path) => {
