9d3873b7d6cf6af6f6e313bab7918b205177b7e5
Nishkal <kashyapnishkal@gmail.com>
2019-06-09 01:29:44 +0530

    added ci-upload script

---
+++ scripts/upload-ci.ts
@@ -0,0 +1,48 @@
+import { Storage } from '@google-cloud/storage';
+import * as fs from 'fs-extra';
+import { printConsoleStatus } from './util';
+import console = require('console');
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = './cloud-storage-key.json';
+const bucketName = 'quarkjs-auto-update';
+const json = JSON.parse(fs.readFileSync('./package.json').toString());
+const version = json.version;
+
+const storage = new Storage({
+    projectId: 'diy-mechatronics'
+});
+
+const paths = [
+    `./build/Quark-win-${version}.exe`,
+    `./build/Quark-win-${version}.exe.blockmap`,
+    `./build/Quark-win-x64-${version}.zip`,
+    './build/latest.yml',
+
+    `./../Quark-electron-linux/build/Quark-linux-amd64-${version}.deb`,
+    `./../Quark-electron-linux/build/Quark-linux-x64-${version}.tar.gz`,
+    `./../Quark-electron-linux/build/Quark-linux-x86_64-${version}.AppImage`,
+    `./../Quark-electron-linux/build/latest-linux.yml`,
+]
+
+paths.map((_path) => {
+    if (fs.existsSync(_path)) {
+        printConsoleStatus(`Found file: ${_path}`, 'info');
+        const file = storage.bucket(bucketName).file(`Quark-${json.version}-test/${_path}`);
+        printConsoleStatus(`Uploading: ${_path}`, 'info');
+        fs.createReadStream(_path)
+            .pipe(file.createWriteStream())
+            .on('error', function (err) {
+                if (err) {
+                    console.error(err);
+                    printConsoleStatus(`Error uploading: ${_path}`, 'danger');
+                }
+            })
+            .on('finish', function () {
+                printConsoleStatus(`Finished file: ${_path}`, 'danger');
+            });
+        return;
+    }
+
+    printConsoleStatus(`File not found: ${_path}`, 'danger');
+});
+
