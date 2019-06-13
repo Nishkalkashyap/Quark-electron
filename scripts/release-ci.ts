2fc62526740c7e82184d15546108fe0d3837ec81
Nishkal <kashyapnishkal@gmail.com>
2019-06-13 11:39:14 +0530

    removed release-ci script

--- scripts/release-ci.ts
+++ scripts/release-ci.ts
@@ -1,4 +1,4 @@
-import { currentBranch, doBucketTransfer, copyContentsToRoot, folderAlreadyExists } from "./util";
+import { currentBranch, doBucketTransfer, folderAlreadyExists } from "./util";
 import * as fs from 'fs-extra';
 import * as path from 'path';
 
@@ -11,28 +11,19 @@ async function release() {
         console.log(`Releasing version: ${packageJson.version}`);
         console.log(`Branch: ${currentBranch}`);
 
-        if (currentBranch == 'master') {
-                const masterFolderName = `Quark-${packageJson.version}`;
-                await copyContentsToRoot('quark-build-nightly.quarkjs.io', masterFolderName);
-                return;
-        }
+        // if (currentBranch == 'master') {
+        //         const masterFolderName = `Quark-${packageJson.version}`;
+        //         await copyContentsToRoot('quark-build-nightly.quarkjs.io', masterFolderName);
+        //         return;
+        // }
 
-        const masterAllFolderName = `Quark-${packageJson.version}`;
-        await copyContentsToRoot('quark-build-nightly-all.quarkjs.io', masterAllFolderName);
+        // const masterAllFolderName = `Quark-${packageJson.version}`;
+        // await copyContentsToRoot('quark-build-nightly-all.quarkjs.io', masterAllFolderName);
 
-
-        const insidersFolderName = `Quark-${releaseJson['insiders']}`;
-        const insidersExists = await folderAlreadyExists('quark-build-insiders.quarkjs.io', insidersFolderName);
-        if (!insidersExists) {
-                await doBucketTransfer('quark-build-nightly-all.quarkjs.io', 'quark-build-insiders.quarkjs.io', insidersFolderName);
-                await copyContentsToRoot('quark-build-insiders.quarkjs.io', insidersFolderName);
-        }
-        
-
-        const stableFolderName = `Quark-${releaseJson['stable']}`;
-        const stableReleaseExists = await folderAlreadyExists('quark-build-stable.quarkjs.io', stableFolderName);
-        if (!stableReleaseExists) {
-                await doBucketTransfer('quark-build-insiders.quarkjs.io', 'quark-build-stable.quarkjs.io', stableFolderName);
-                await copyContentsToRoot('quark-build-stable.quarkjs.io', stableFolderName)
-        }
+        // const stableFolderName = `Quark-${releaseJson['stable']}`;
+        // const stableReleaseExists = await folderAlreadyExists('quark-release.quarkjs.io', stableFolderName);
+        // if (!stableReleaseExists) {
+        //         await doBucketTransfer('quark-builds.quarkjs.io', 'quark-build-stable.quarkjs.io', stableFolderName);
+        //         await copyContentsToRoot('quark-build-stable.quarkjs.io', stableFolderName)
+        // }
 }
