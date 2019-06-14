97a7e22313f2637647fcab535ecea671b768343b
Nishkal <kashyapnishkal@gmail.com>
2019-06-14 16:19:51 +0530

    removed un necessary packages from package.json

--- scripts/prepare-tar.ts
+++ scripts/prepare-tar.ts
@@ -1,6 +1,4 @@
-import * as fs from 'fs-extra';
 import * as path from 'path';
-import * as archiver from 'archiver';
 import { printConsoleStatus } from './util';
 import console = require('console');
 import * as tar from 'tar';
@@ -27,43 +25,4 @@ function makeZip(inDir: string, outFilePath: string, prefix: string) {
         printConsoleStatus(`Tarball Error ${outFilePath}`, 'danger');
         console.log(err);
     });
-}
-
-function makeeZip(inDir: string, outFilePath: string) {
-    var output = fs.createWriteStream(outFilePath);
-    const archive = archiver('zip', {
-        zlib: { level: 9 }
-    });
-
-    output.on('close', async function () {
-        printConsoleStatus(archive.pointer() + ' total bytes', 'info');
-        printConsoleStatus('archiver has been finalized and the output file descriptor has closed.', 'info');
-    });
-
-    output.on('end', function () {
-        printConsoleStatus('Data has been drained', 'info');
-    });
-
-    archive.on('warning', function (err) {
-        if (err.code === 'ENOENT') {
-            // log warning
-        } else {
-            // throw error
-            throw err;
-        }
-    });
-
-    // good practice to catch this error explicitly
-    archive.on('error', function (err) {
-        throw err;
-    });
-
-    archive.pipe(output);
-
-    // archive.directory(inDir, false);
-    archive.glob('**', {
-        cwd: inDir,
-        ignore: ['**/node_modules/**']
-    })
-    archive.finalize();
 }
