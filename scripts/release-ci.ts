828c193e39a073292c27f06ce252c627d1ed66c4
Nishkal <kashyapnishkal@gmail.com>
2019-06-14 15:16:47 +0530

    will throw errors on duplicate releases

--- scripts/release-ci.ts
+++ scripts/release-ci.ts
@@ -1,4 +1,4 @@
-import { currentBranch, doBucketTransfer, cleanDirectory, printConsoleStatus } from "./util";
+import { currentBranch, doBucketTransfer, cleanDirectory, folderAlreadyExists, branches } from "./util";
 import * as fs from 'fs-extra';
 import * as path from 'path';
 
@@ -10,22 +10,32 @@ release().catch((err) => {
 });
 async function release() {
 
-        const copyBranchName = 'master-all';
-        // const copyBranchName = 'master';
+        const copyBranchName: branches = 'master-all';
 
         const releaseJson = fs.readJsonSync(path.join(__dirname, './release.json'));
         const packageJson = fs.readJsonSync('./package.json');
         console.log(`Releasing version: ${packageJson.version}`);
         console.log(`Branch: ${currentBranch}`);
 
+
         const insidersFolderCopyFrom = `Quark-${copyBranchName}-${releaseJson['insiders']}`;
         const insidersFolderCopyTo = `Quark-insiders-${releaseJson['insiders']}`;
+        const insidersAlreadyExists = await folderAlreadyExists('quark-release.quarkjs.io', insidersFolderCopyTo);
+
+        if (insidersAlreadyExists) {
+                throw Error(`Error: Release Quark-insiders-${packageJson.version} already exists.`);
+        }
         await doBucketTransfer('quark-builds.quarkjs.io', 'quark-release.quarkjs.io', insidersFolderCopyFrom, insidersFolderCopyTo, false);
         await cleanDirectory('quark-release.quarkjs.io', 'insiders');
         await doBucketTransfer('quark-release.quarkjs.io', 'quark-release.quarkjs.io', insidersFolderCopyTo, 'insiders', true);
 
         const stableFolderCopyFrom = `Quark-insiders-${releaseJson['stable']}`;
         const stableFolderCopyTo = `Quark-stable-${releaseJson['stable']}`;
+        const stableAlreadyExists = await folderAlreadyExists('quark-release.quarkjs.io', stableFolderCopyTo);
+
+        if (stableAlreadyExists) {
+                throw Error(`Error: Release Quark-stable-${packageJson.version} already exists.`);
+        }
         await doBucketTransfer('quark-release.quarkjs.io', 'quark-release.quarkjs.io', stableFolderCopyFrom, stableFolderCopyTo, false);
         await cleanDirectory('quark-release.quarkjs.io', 'stable');
         await doBucketTransfer('quark-release.quarkjs.io', 'quark-release.quarkjs.io', stableFolderCopyTo, 'stable', true);
