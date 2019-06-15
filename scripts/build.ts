5878b563b8d2d80c43670d28f0fd665f673836a9
Nishkal <kashyapnishkal@gmail.com>
2019-06-15 16:05:43 +0530

    added macOS CSC_LINK

--- scripts/build.ts
+++ scripts/build.ts
@@ -7,6 +7,11 @@ if ((currentBranch == 'master-all' && process.env.CI) || process.platform == 'da
     console.log(`Environment variables Initiated`);
     console.log(fs.existsSync('./dev-assets/prod.env'));
     dotenv.config({ path: './dev-assets/prod.env' });
+    console.log(process.env.CSC_LINK);
+    if (process.platform == 'darwin') {
+        process.env.CSC_LINK = './dev-assets/envValue.txt';
+        console.log(process.env.CSC_LINK);
+    }
 }
 
 // if (os.platform() == 'linux') {
