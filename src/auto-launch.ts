11d16e324106f395e80f6755899df0f81781479f
Nishkal <kashyapnishkal@gmail.com>
2019-07-14 13:24:22 +0530

    removed auto-launcher

--- src/auto-launch.ts
+++ src/auto-launch.ts
@@ -1,27 +1,27 @@
-import AutoLaunch from 'auto-launch';
-import { mainProcessData, handleEmptyPromise } from './util';
-import { app } from 'electron';
+// import AutoLaunch from 'auto-launch';
+// import { mainProcessData, handleEmptyPromise } from './util';
+// import { app } from 'electron';
 
 export async function enableAutoLaunch() {
-    var autoLauncher = new AutoLaunch({
-        name: 'Quark'
-    });
+    // var autoLauncher = new AutoLaunch({
+    //     name: 'Quark'
+    // });
 
-    if (mainProcessData.launchAppOnLogin && app.isPackaged) {
-        await handleEmptyPromise(autoLauncher.enable());
-    } else {
-        await handleEmptyPromise(autoLauncher.disable());
-    }
+    // if (mainProcessData.launchAppOnLogin && app.isPackaged) {
+    //     await handleEmptyPromise(autoLauncher.enable());
+    // } else {
+    //     await handleEmptyPromise(autoLauncher.disable());
+    // }
 
-    autoLauncher.isEnabled()
-        .then(function (isEnabled) {
-            if (isEnabled) {
-                return;
-            }
-            autoLauncher.enable();
-        })
-        .catch(function (err) {
-            // handle error
-            console.error(err);
-        });
+    // autoLauncher.isEnabled()
+    //     .then(function (isEnabled) {
+    //         if (isEnabled) {
+    //             return;
+    //         }
+    //         autoLauncher.enable();
+    //     })
+    //     .catch(function (err) {
+    //         // handle error
+    //         console.error(err);
+    //     });
 }
