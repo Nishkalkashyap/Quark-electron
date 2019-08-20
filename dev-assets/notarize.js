49def09fc686a70862c9fe36bc1e43833409a826
Nishkal <kashyapnishkal@gmail.com>
2019-08-20 23:39:45 +0530

    fixed notarization bug

--- dev-assets/notarize.js
+++ dev-assets/notarize.js
@@ -13,9 +13,7 @@ exports.default = async function notarizing(context) {
   return await notarize({
     appBundleId: 'in.nishkal',
     appPath: `${appOutDir}/${appName}.app`,
-    appleId: 'fdsfsdfdsf',
-    appleIdPassword: 'fdsfdsfdsf',
-    // appleId: process.env.APPLEID,
-    // appleIdPassword: process.env.APPLEIDPASS,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
   });
 };
